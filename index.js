b0d5ec62da7affa4540ed8ead7c8bcd056c3db7a
ellasig <103430174+ellasig@users.noreply.github.com>
2023-03-13 15:00:15 +0200

    cat pictures

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-console.log("Hello world!")
+'use strict';
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.static("public"))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get("/catinfo", (req, res) => {
+    const cat = {
+      name: "Frank",
+      birthdate: "2010-12-25",
+      weight: 5,
+    };
+    res.json(cat);
+  });
 
