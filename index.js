547ce3726b06101875058112ecea3a9c2b66be99
ellasig <103430174+ellasig@users.noreply.github.com>
2023-03-13 14:03:58 +0200

    added new branch

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const _ = require("lodash");
 const output = _.camelCase("something")
 
 console.log(output)
+console.log("kukkuu");
+console.log("taas jotain uutta");
+console.log("kukkuu");
+console.log("moi");
+console.log("haloo");
 
+// ctrl-c tappaa nodemonin
---
+++ readme.md
@@ -0,0 +1,4 @@
+# webkurssi 2023 examples
+
+
+Opettajan tuntien esimerkit
